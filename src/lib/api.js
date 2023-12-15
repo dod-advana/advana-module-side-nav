75f193729df788410fc92a16f26c072d8794cd1d
Frankie <amaya_franklin@bah.com>
2023-12-15 13:08:16 -0500

    testing axios fix

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,23 +1,27 @@
-import axios from 'axios';
+const axios = require("axios");
 
 export async function getLinks() {
-	try {
-		let data;
+  try {
+    let data;
 
-		try {
-			data = JSON.parse(sessionStorage.getItem('advana-mega-menu'));
-		} catch (err) {
-			console.error(err);
-		}
+    try {
+      data = JSON.parse(sessionStorage.getItem("advana-mega-menu"));
+    } catch (err) {
+      console.error(err);
+    }
 
-		if (!data) {
-			const response = await axios.get(window?.__env__?.REACT_APP_MEGA_MENU_ENDPOINT || process.env.REACT_APP_MEGA_MENU_ENDPOINT, { withCredentials: true });
-			data = response?.data;
-			sessionStorage.setItem('advana-mega-menu', JSON.stringify(data));
-		}
-		return data?.links || {};
-	} catch (err) {
-		console.error(err);
-		return {};
-	}
+    if (!data) {
+      const response = await axios.get(
+        window?.__env__?.REACT_APP_MEGA_MENU_ENDPOINT ||
+          process.env.REACT_APP_MEGA_MENU_ENDPOINT,
+        { withCredentials: true }
+      );
+      data = response?.data;
+      sessionStorage.setItem("advana-mega-menu", JSON.stringify(data));
+    }
+    return data?.links || {};
+  } catch (err) {
+    console.error(err);
+    return {};
+  }
 }
